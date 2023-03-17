130f6959bb334210240b3d19c7a02ef63988bb90
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-03-17 21:53:06 +0530

    Update SeivePrime.cpp

--- SeivePrime.cpp
+++ SeivePrime.cpp
@@ -1,8 +1,8 @@
 const ll nax = 1e5 + 5 ;//Check constraints from question again
 //Global Variable ahead make sure to reinitialise them in case of multiple TC's
  
-vector<ll> primes ;
-vector<bool> sieve(nax , true) ;
+vector<ll> primes ;   // all the prime number stores in this 
+vector<bool> sieve(nax , true) ; // this is just as vis array;
  
 void pre(){
     sieve[0] = sieve[1] = false ;
@@ -15,5 +15,7 @@ void pre(){
         }
     }
 }
+
+cout<< primes[x] <<endl; // prime
 
 // and just call it before solve()
