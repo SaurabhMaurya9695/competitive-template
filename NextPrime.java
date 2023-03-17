20b08a2f9ff7da6e718d8d3dc267664b4693cf42
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-03-17 21:44:16 +0530

    giving the next prime to the number

---
+++ NextPrime.java
@@ -0,0 +1,28 @@
+public static long nextPrime(long i) {
+		// TODO Auto-generated method stub
+		if(i<=2) {
+			return 2;
+		}if(i<=3) {
+			return 3;
+		}
+		if(i<=5) {
+			return 5;
+		}if(i<=7) {
+			return 7;
+		}
+		if(i<=10) {
+			i=11;
+		}
+		if(i%2==0) {
+			i++;
+		}
+		while(true) {
+			if(i%5==0) {
+				i+=2;
+				continue;
+			}
+			if(isPrime(i)) {
+				return i;
+			}i+=2;
+		}
+	}
